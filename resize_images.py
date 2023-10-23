382e59652cdaee90bc672dbf4d5136f21405e141
etm22 <easytubemastery@gmail.com>
2023-10-23 10:11:03 +0400

    add website text

--- resize_images.py
+++ resize_images.py
@@ -1,8 +1,8 @@
-from PIL import Image
+from PIL import Image,ImageDraw,ImageFont
 import os
 
 
-def adjustImage(in_path,out_path):
+def adjustImage(in_path,out_path,add_text):
     desired_height = 1080
 
     # Resize each image to ensure its height fits into a 16:9 frame
@@ -16,16 +16,35 @@ def adjustImage(in_path,out_path):
     # Pad each resized image to the maximum width
     padded_img = Image.new("RGB", (1920, desired_height), color=(0, 0, 0))
     padded_img.paste(resized_img, ((1920 - resized_img.width) // 2, 0))
-    padded_img.save(out_path)
 
+    if add_text:
+        # Add text at the bottom
+        text_to_add="Free AI Image Generator: www.uncashy.com"
+        draw = ImageDraw.Draw(padded_img)
+        text_color = (255, 255, 255)  # White text color
+        text_font = ImageFont.truetype("arial.ttf", 36)  # You can specify the font and size
+        text_width, text_height = draw.textsize(text_to_add, font=text_font)
+        text_position = ((1920 - text_width) // 2, 10)
+
+        background_position = (text_position[0] - 5, text_position[1] - 5)
+        background_size = (text_width + 10, text_height + 10)
+        draw.rectangle([background_position, (background_position[0] + background_size[0], background_position[1] + background_size[1])], fill=(0, 0, 0))
+        
+        draw.text(text_position, text_to_add, fill=text_color, font=text_font)
+
+    padded_img.save(out_path)
 
 in_path = "outputs/images"
 out_path = "outputs/images_resized"
 images = [f for f in os.listdir(in_path) if f.lower().endswith('.jpeg')]
 
-for img in images:
+for idx in range(len(images)):
+    img = images[idx]
     try:
-        adjustImage(f"{in_path}/{img}",f"{out_path}/{img}")
+        if(idx%5 == 0):
+            adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",True)
+        else:
+            adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",False)
     except:
         print(f"Failed to resize image: {img}")
         pass
