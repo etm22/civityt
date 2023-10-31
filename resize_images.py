ff1832c9cf10c81f0236c879aee58dd299a220f6
etm22 <easytubemastery@gmail.com>
2023-10-31 12:39:23 +0400

    est

--- resize_images.py
+++ resize_images.py
@@ -2,8 +2,7 @@ from PIL import Image,ImageDraw,ImageFont
 import os
 
 
-def adjustImage(in_path,out_path,add_text):
-    desired_height = 1080
+def adjustImage(in_path,out_path,add_text,desired_height, desired_width):
 
     # Resize each image to ensure its height fits into a 16:9 frame
     img = Image.open(in_path)
@@ -14,17 +13,17 @@ def adjustImage(in_path,out_path,add_text):
     resized_img = img.resize((new_width, desired_height))
 
     # Pad each resized image to the maximum width
-    padded_img = Image.new("RGB", (1920, desired_height), color=(0, 0, 0))
-    padded_img.paste(resized_img, ((1920 - resized_img.width) // 2, 0))
+    padded_img = Image.new("RGB", (desired_width, desired_height), color=(0, 0, 0))
+    padded_img.paste(resized_img, ((desired_width - resized_img.width) // 2, 0))
 
     if add_text:
         # Add text at the bottom
-        text_to_add="Free AI Image Generator: www.uncashy.com"
+        text_to_add="Top AI generated images"
         draw = ImageDraw.Draw(padded_img)
         text_color = (255, 255, 255)  # White text color
-        text_font = ImageFont.truetype("arial.ttf", 36)  # You can specify the font and size
+        text_font = ImageFont.truetype("arial.ttf", 40)  # You can specify the font and size
         text_width, text_height = draw.textsize(text_to_add, font=text_font)
-        text_position = ((1920 - text_width) // 2, 10)
+        text_position = ((desired_width - text_width) // 2, 10)
 
         background_position = (text_position[0] - 5, text_position[1] - 5)
         background_size = (text_width + 10, text_height + 10)
@@ -34,17 +33,18 @@ def adjustImage(in_path,out_path,add_text):
 
     padded_img.save(out_path)
 
-in_path = "outputs/images"
+in_path = "Lookbook"
 out_path = "outputs/images_resized"
-images = [f for f in os.listdir(in_path) if f.lower().endswith('.jpeg')]
+images = [f for f in os.listdir(in_path) if f.lower().endswith('.png')]
 
 for idx in range(len(images)):
     img = images[idx]
     try:
-        if(idx%5 == 0):
-            adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",True)
-        else:
-            adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",False)
+        adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",False, 960,1706 )
+        # if(idx%5 == 0):
+            # adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",False)
+        # else:
+            # adjustImage(f"{in_path}/{img}",f"{out_path}/{img}",False, 1920, 1080)
     except:
         print(f"Failed to resize image: {img}")
         pass
