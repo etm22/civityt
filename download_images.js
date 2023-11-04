e94b0f26ace972c9f5150c40cfa710a83771f155
etm22 <easytubemastery@gmail.com>
2023-11-04 19:54:13 +0400

    parallel download

--- download_images.js
+++ download_images.js
@@ -1,5 +1,7 @@
-const fs = require("fs/promises");
-const axios = require("axios");
+import * as fs from "fs/promises";
+import axios from "axios";
+import pLimit from "p-limit";
+const limit = pLimit(10);
 
 (async () => {
   const config = JSON.parse(await fs.readFile("data/config.json", "utf-8"));
@@ -15,17 +17,19 @@ const axios = require("axios");
   shuffleArray(imageUrls);
   shuffleArray(imageUrls);
 
-  for (let idx = 0; idx < num_of_images_to_download; idx++) {
-    const url = imageUrls[idx];
-    try {
-      const response = await axios.get(url, { responseType: "arraybuffer" });
-      await fs.writeFile(`outputs/images/${idx}.jpeg`, response.data);
-      console.log(`${idx}/${num_of_images_to_download} downloaded`);
-    } catch (error) {
-      console.log("Failed to download: ", url);
-    }
-  }
+  imageUrls
+    .slice(0, num_of_images_to_download)
+    .map((url) => limit(() => downloadAndSaveImage(url)));
 })();
+
+async function downloadAndSaveImage(url) {
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    await fs.writeFile(`outputs/images/${Date.now()}.jpeg`, response.data);
+  } catch (error) {
+    console.log("Failed to download: ", url);
+  }
+}
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
