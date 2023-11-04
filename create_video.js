66ab826d87e7de9472a006b301878e0f3483a9f9
etm22 <easytubemastery@gmail.com>
2023-11-04 20:11:15 +0400

    test render

--- create_video.js
+++ create_video.js
@@ -12,42 +12,45 @@ const path = require("path");
   const video_length_in_sec =
     (resized_images.length - 1) * config.image_duration_in_sec;
 
-  // select random video
-  const videos = JSON.parse(await fs.readFile("data/musics.json", "utf-8"));
-  shuffleArray(videos);
-  shuffleArray(videos);
-  shuffleArray(videos);
-  const selectedVideo = videos[0];
+  // // select random video
+  // const videos = JSON.parse(await fs.readFile("data/musics.json", "utf-8"));
+  // shuffleArray(videos);
+  // shuffleArray(videos);
+  // shuffleArray(videos);
+  // const selectedVideo = videos[0];
 
-  // download audios
-  const songs = selectedVideo.songs;
-  shuffleArray(songs);
-  shuffleArray(songs);
-  shuffleArray(songs);
-  shuffleArray(songs);
+  // // download audios
+  // const songs = selectedVideo.songs;
+  // shuffleArray(songs);
+  // shuffleArray(songs);
+  // shuffleArray(songs);
+  // shuffleArray(songs);
 
-  let totalMusicDuration = 0;
-  let txt = "";
-  for (let idx = 0; idx < songs.length; idx++) {
-    if (totalMusicDuration >= video_length_in_sec) break;
-    const song = songs[idx];
+  // let totalMusicDuration = 0;
+  // let txt = "";
+  // for (let idx = 0; idx < songs.length; idx++) {
+  //   if (totalMusicDuration >= video_length_in_sec) break;
+  //   const song = songs[idx];
 
-    execSync(
-      `yt-dlp "${selectedVideo.url}" --downloader ffmpeg --downloader-args "ffmpeg_i:-ss ${song.start} -to ${song.end}" --extract-audio --audio-format mp3 -o "outputs/musics/${idx}.mp3"`
-    );
-    txt += `file '${idx}.mp3'\n`;
-    totalMusicDuration += song.duration;
-  }
+  //   execSync(
+  //     `yt-dlp "${selectedVideo.url}" --downloader ffmpeg --downloader-args "ffmpeg_i:-ss ${song.start} -to ${song.end}" --extract-audio --audio-format mp3 -o "outputs/musics/${idx}.mp3"`
+  //   );
+  //   txt += `file '${idx}.mp3'\n`;
+  //   totalMusicDuration += song.duration;
+  // }
 
-  // merge musics together
-  await fs.writeFile("outputs/musics/music.txt", txt);
-  execSync(
-    `ffmpeg -f concat -safe 0 -i "${outputDirectory}/musics/music.txt" -c copy "${outputDirectory}/musics/music.mp3"`
-  );
+  // // merge musics together
+  // await fs.writeFile("outputs/musics/music.txt", txt);
+  // execSync(
+  //   `ffmpeg -f concat -safe 0 -i "${outputDirectory}/musics/music.txt" -c copy "${outputDirectory}/musics/music.mp3"`
+  // );
 
   // create final video + add audio
+  // execSync(
+  //   `ffmpeg -framerate 1/${config.image_duration_in_sec} -pattern_type glob -i "${outputDirectory}/images_resized/*.jpeg" -i "${outputDirectory}/musics/music.mp3" -c:v libx264 -r 1 -pix_fmt yuv420p -c:a aac -strict experimental -shortest "${outputDirectory}/video.mp4"`
+  // );
   execSync(
-    `ffmpeg -framerate 1/${config.image_duration_in_sec} -pattern_type glob -i "${outputDirectory}/images_resized/*.jpeg" -i "${outputDirectory}/musics/music.mp3" -c:v libx264 -r 1 -pix_fmt yuv420p -c:a aac -strict experimental -shortest "${outputDirectory}/video.mp4"`
+    `ffmpeg -framerate 1/${config.image_duration_in_sec} -pattern_type glob -i "${outputDirectory}/images_resized/*.jpeg" -c:v libx264 -r 1 -pix_fmt yuv420p "${outputDirectory}/video.mp4"`
   );
 })();
 
